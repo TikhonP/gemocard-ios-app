6a8794c1595931cf646eeaa2a16a73b137fb4419
TikhonP <tikhonp@users.noreply.github.com>
2022-10-04 14:00:14 +0300

    Initial Commit

---
+++ Medsenger Gemocard/Medsenger_GemocardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Medsenger_GemocardApp.swift
+//  Medsenger Gemocard
+//
+//  Created by Tikhon Petrishchev on 04.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct Medsenger_GemocardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
